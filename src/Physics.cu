cafd318ad000d0b132f34abad4e2cd1c821fbe0b
Ilya Kostin <yakostin2010@yandex.ru>
2021-03-03 01:56:08 +0300

    Update Physics.cu and main.cpp

--- src/Physics.cu
+++ src/Physics.cu
@@ -3,21 +3,20 @@
 //
 
 #include "Physics.hpp"
-#include <float.h>
 
 __device__ bool intersect(const Body &l, const Body &r) {
     float X = l.x - r.x, Y = l.y - r.y, R = r.radius + l.radius;
     return (X * X + Y * Y - R * R) < 0.f;
 }
 
-__device__ void merge(Body *body_vec, Body *result, int bodies_count) {
+__device__ void merge(Body *body_vec, int bodies_count) {
     int idx = blockIdx.x * blockDim.x + threadIdx.x;
-    Body current_body = body_vec[idx];
     bool delete_this = false;
 
     if (idx > bodies_count)
         return;
 
+    Body current_body = body_vec[idx];
     if (current_body.mass == 0.f)
         return;
 
@@ -55,10 +54,10 @@ __device__ void merge(Body *body_vec, Body *result, int bodies_count) {
         current_body.radius = 0.f;
     }
 
-    result[idx] = current_body;
+    body_vec[idx] = current_body;
 }
 
-__device__ void update_positions(Body *body_vec, Body *result, size_t bodies_count) {
+__device__ void update_positions(Body *body_vec, size_t bodies_count) {
     constexpr auto step = 0.2f;
     int idx = blockIdx.x * blockDim.x + threadIdx.x;
     if (idx > bodies_count)
@@ -66,7 +65,7 @@ __device__ void update_positions(Body *body_vec, Body *result, size_t bodies_cou
 
     float Fx = 0., Fy = 0.;
 
-    Body &current_body = body_vec[idx];
+    Body current_body = body_vec[idx];
     for (int i = 0; i < bodies_count; i++) {
         auto tmp = body_vec[i];
 
@@ -78,24 +77,22 @@ __device__ void update_positions(Body *body_vec, Body *result, size_t bodies_cou
         auto F = 6.674184 * 10e-9 * (current_body.mass * tmp.mass) / D2;
         auto D = sqrt(D2);
 
+        if(D < (tmp.radius + current_body.radius))
+            continue;
+
         Fx += F * X / D;
         Fy += F * Y / D;
     }
 
-    result[idx].x += current_body.speed_x * step;
-    result[idx].y += current_body.speed_y * step;
-    result[idx].speed_x += Fx * step / current_body.mass;
-    result[idx].speed_y += Fy * step / current_body.mass;
+    body_vec[idx].x += current_body.speed_x * step;
+    body_vec[idx].y += current_body.speed_y * step;
+    body_vec[idx].speed_x += Fx * step / current_body.mass;
+    body_vec[idx].speed_y += Fy * step / current_body.mass;
 }
 
-__global__ void update_gpu_bodies(Body *body_vec, Body *result, int bodies_count) {
-    int idx = blockIdx.x * blockDim.x + threadIdx.x;
-    merge(body_vec, result, bodies_count);
-
-    __syncthreads();
-
-    body_vec[idx] = result[idx];
-    update_positions(body_vec, result, bodies_count);
+__global__ void update_gpu_bodies(Body *body_vec, int bodies_count) {
+    merge(body_vec, bodies_count);
+    update_positions(body_vec, bodies_count);
 }
 
 Physics::Physics() : gpu_bodies_vec_(nullptr) {}
@@ -118,15 +115,6 @@ void Physics::update(std::vector<Body> &bodies) {
     dim3 threads = dim3(64, 1);
     dim3 blocks = bodies.size() / threads.x != 0 ? dim3((bodies.size() / threads.x) + 1, 1) : dim3(1, 1);
 
-    // TMP
-    Body *gpu_result;
-    cudaMalloc(&gpu_result, sizeof(Body) * bodies.size());
-
-    cudaMemcpy(gpu_result, gpu_bodies_vec_, sizeof(Body) * bodies.size(), cudaMemcpyDeviceToDevice);
-    update_gpu_bodies<<<blocks, threads>>>(gpu_bodies_vec_, gpu_result, bodies.size());
-    cudaMemcpy(bodies.data(), gpu_result, sizeof(Body) * bodies.size(), cudaMemcpyDeviceToHost);
-    cudaMemcpy(gpu_bodies_vec_, gpu_result, sizeof(Body) * bodies.size(), cudaMemcpyDeviceToDevice);
-
-    // TMP
-    cudaFree(gpu_result);
+    update_gpu_bodies<<<blocks, threads>>>(gpu_bodies_vec_, bodies.size());
+    cudaMemcpy(bodies.data(), gpu_bodies_vec_, sizeof(Body) * bodies.size(), cudaMemcpyDeviceToHost);
 }
