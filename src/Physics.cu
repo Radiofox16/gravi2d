94a3650fd999f3e035fe075961afa1787735df71
Ilya Kostin <yakostin2010@yandex.ru>
2021-05-30 22:57:32 +0300

    trying to use shared memory, but it didn't work

--- src/Physics.cu
+++ src/Physics.cu
@@ -6,54 +6,63 @@
 #include <algorithm>
 #include <limits>
 
+constexpr int THREAD_WARP_SIZE = 32;
+
 __device__ bool intersect(const Body &l, const Body &r) {
     float X = l.x - r.x, Y = l.y - r.y, R = r.radius + l.radius;
     return (X * X + Y * Y - R * R) < 0.f;
 }
 
+__device__ void merge_two_bodies(Body &a, const Body &b) {
+    a.x = a.radius > b.radius ? a.x : b.x;
+    a.y = a.radius > b.radius ? a.y : b.y;
+
+    a.radius = sqrt(a.radius * a.radius + b.radius * b.radius);
+
+    auto mass_sum = b.mass + a.mass;
+    a.speed_x =
+            a.speed_x * (a.mass / mass_sum) + (b.speed_x) * (b.mass / mass_sum);
+    a.speed_y =
+            a.speed_y * (a.mass / mass_sum) + (b.speed_y) * (b.mass / mass_sum);
+
+    a.mass = mass_sum;
+}
+
 __device__ void merge(Body *body_vec, int bodies_count) {
     int idx = blockIdx.x * blockDim.x + threadIdx.x;
-    bool delete_this = false;
-
     if (idx > bodies_count)
         return;
 
+    __shared__ Body cached_bodies[THREAD_WARP_SIZE];
     Body current_body = body_vec[idx];
-    if (current_body.mass == 0.f)
-        return;
+    cached_bodies[threadIdx.x] = body_vec[idx];
+    __syncwarp();
 
-    for (int i = 0; i < idx; i++) {
-        if (intersect(current_body, body_vec[i])) {
-            delete_this = true;
-            break;
+    for (int i = 0; i < THREAD_WARP_SIZE; i++) {
+        if (intersect(current_body, cached_bodies[i])) {
+            if (current_body.id < cached_bodies[i].id)
+                current_body.mass = 0.f;
+            else
+                merge_two_bodies(current_body, cached_bodies[i]);
         }
     }
 
-    if (!delete_this) {
-        for (int i = idx + 1; i < bodies_count; i++) {
-            auto tmp = body_vec[i];
+    for (int i = blockIdx.x + 1; i < gridDim.x; i++) {
+        cached_bodies[threadIdx.x] = body_vec[i * blockDim.x + threadIdx.x];
+        __syncwarp();
 
+        for (int j = 0; j < THREAD_WARP_SIZE; j++) {
+            auto tmp = cached_bodies[j];
             if (tmp.mass == 0.f)
                 continue;
 
             if (intersect(current_body, tmp)) {
-                current_body.x = current_body.radius > tmp.radius ? current_body.x : tmp.x;
-                current_body.y = current_body.radius > tmp.radius ? current_body.y : tmp.y;
-
-                current_body.radius = sqrt(current_body.radius * current_body.radius + tmp.radius * tmp.radius);
-
-                auto mass_sum = tmp.mass + current_body.mass;
-                current_body.speed_x =
-                        current_body.speed_x * (current_body.mass / mass_sum) + (tmp.speed_x) * (tmp.mass / mass_sum);
-                current_body.speed_y =
-                        current_body.speed_y * (current_body.mass / mass_sum) + (tmp.speed_y) * (tmp.mass / mass_sum);
-
-                current_body.mass = mass_sum;
+                if (current_body.id < cached_bodies[i].id)
+                    current_body.mass = 0.f;
+                else
+                    merge_two_bodies(current_body, cached_bodies[i]);
             }
         }
-    } else {
-        current_body.mass = 0.f;
-        current_body.radius = 0.f;
     }
 
     body_vec[idx] = current_body;
@@ -136,9 +145,26 @@ void Physics::load(std::vector<Body> &bodies) {
 }
 
 void Physics::update(std::vector<Body> &bodies) {
-    dim3 threads = dim3(64, 1);
+    if (!bodies.empty()) {
+        std::vector<Body> tmp;
+        tmp.reserve(((bodies.size() / THREAD_WARP_SIZE) + 1) * THREAD_WARP_SIZE);
+
+        for (const auto &b: bodies) {
+            if (b.mass != 0.f)
+                tmp.push_back(b);
+        }
+
+        for (int i = 0; i < THREAD_WARP_SIZE - (bodies.size() % THREAD_WARP_SIZE); i++) {
+            tmp.push_back({});
+        }
+
+        bodies = tmp;
+    }
+
+    dim3 threads = dim3(THREAD_WARP_SIZE, 1);
     dim3 blocks = bodies.size() / threads.x != 0 ? dim3((bodies.size() / threads.x) + 1, 1) : dim3(1, 1);
 
+    cudaMemcpy(gpu_bodies_vec_, bodies.data(), sizeof(Body) * bodies.size(), cudaMemcpyHostToDevice);
     update_gpu_bodies<<<blocks, threads>>>(gpu_bodies_vec_, bodies.size());
     cudaMemcpy(bodies.data(), gpu_bodies_vec_, sizeof(Body) * bodies.size(), cudaMemcpyDeviceToHost);
 }
